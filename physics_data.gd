ab7e3b1e9b95e46fa5f9f2ec8cbfb02dd4f6a35d
Logic Monkey <logic.monkey@protonmail.com>
2023-12-29 12:44:49 -0600

    Basic physics stuff.

---
+++ physics_data.gd
@@ -0,0 +1,61 @@
+@icon("phys.svg")
+extends Resource
+class_name PLA_PhysData
+
+## Movement categories sorted by relative speed.
+enum {\
+		IDLE = 			0,
+		SKIDDING = 		1,
+		WALKING = 		2,
+		WALKING_FAST = 	3,
+		RUNNING = 		4,
+		RUNNING_FAST = 	5,
+		BLITZ = 		6,
+		AIRBORN = 		7,
+	}
+	
+@export_category("Physics and Input Constants")
+@export_group("Stick Thresholds", "stick_")
+@export_range(0.0,1.0) var stick_walk_threshold : float = 0.2
+@export_range(0.0,1.0) var stick_crouch_threshold : float = 0.8
+@export_range(0.0,1.0) var stick_attack_threshold : float = 0.8
+
+@export_group("Idle", "idle_")
+@export var idle_gravity : float = 				  0.5
+@export var idle_drag : float = 				 5
+
+@export_group("Walking", "walk_")
+@export var walk_gravity : float = 				  3
+@export var walk_drag : float = 				  1.75
+@export var walk_acceleration : float = 		  1
+@export var walk_turn_acceleration : float = 	  2
+@export var walk_upper_bound : float = 			 10
+
+@export_group("Running")
+@export_subgroup("Running", "run_")
+@export var run_gravity : float = 				  5
+@export var run_drag : float = 					  0.5
+@export var run_acceleration : float = 			  1.5
+@export var run_velocity_burst : float = 		  3
+@export var run_lower_bound : float = 			 20
+@export_subgroup("Skidding", "skid_")
+@export var skid_acceleration : float = 		  1.5
+@export var skid_lower_bound : float = 			 15
+
+@export_group("Airborn")
+@export var jump_strength : float = 			 15
+@export_subgroup("Airborn", "airborn_")
+@export var airborn_rising_gravity : float = 	  1
+@export var airborn_falling_gravity : float =	  5
+@export var airborn_drag : float = 				  0.4
+@export_subgroup("Flapping", "flap_")
+@export var flap_acceleration : float = 		  1.5
+@export var flap_decay : float = 				  0.75
+
+@export_group("Attack", "attack_")
+@export var attack_pushback_strength : float = 	 10
+
+@export_group("Damage", "damage_")
+@export var damage_knockback_force: float = 	  1.5
+@export var damage_knockback_vector: Vector2 = Vector2(-1, -0.5)
+@export var damage_hit_force_multiplier: float =  7
---
+++ shared_physics.gd
@@ -0,0 +1,48 @@
+@icon("phys.svg")
+extends Node
+class_name SharedPhysics
+
+@export var base_drag := 0.1
+@export var base_gravity := 3.0
+@onready var body = owner as CharacterBody2D
+
+@export var constants : PLA_PhysData
+
+const SANITY_MULTIPLIER := 30.0
+
+@export var current_gravity_multiplier := 1.0
+@export var current_drag_multiplier := 1.0
+@export var current_acceleration_multiplier := 1.0
+
+static var scale := 24.0
+static func _static_init():
+	var bb = load("res://blackboard.tres")
+	if not bb or not "physics_scale" in bb: return
+	scale = bb.physics_scale
+
+var floor_movement_normal := Vector2.RIGHT
+var velocity_cache = Vector2.ZERO
+
+func move_owner(delta: float, acceleration: Vector2 = Vector2.ZERO,\
+		drag_multiplier: float = 1, gravity_multiplier: float = 1, airborn = false):
+	
+	acceleration.y += base_gravity * current_gravity_multiplier * gravity_multiplier * delta * SANITY_MULTIPLIER
+	acceleration *= current_acceleration_multiplier * scale
+	acceleration -= velocity_cache * base_drag * drag_multiplier * current_drag_multiplier * delta * SANITY_MULTIPLIER
+	velocity_cache += acceleration / 2
+	# -- From Godot 3 --. Manage airbornness in some other way.
+	#var snapvec = Vector2.DOWN
+	#if airborn: snapvec = Vector2.ZERO
+	body.velocity = velocity_cache
+	body.move_and_slide()
+	velocity_cache = body.get_real_velocity()
+	velocity_cache += acceleration / 2
+	if body.is_on_floor():
+		floor_movement_normal = body.get_floor_normal().rotated(1.5708)
+	else: 
+		floor_movement_normal = Vector2.RIGHT
+	
+
+
+func burst_accelerate(acceleration: Vector2):
+	velocity_cache += acceleration * scale
